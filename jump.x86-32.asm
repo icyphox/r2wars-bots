db27b57afc04a89cc140a7c8a56323ef84c54201
Anirudh Oppiliappan <x@icyphox.sh>
2020-09-05 19:41:21 +0530

    jump: now mobile

--- jump.x86-32.asm
+++ jump.x86-32.asm
@@ -1,17 +1,13 @@
 ; when i say jump -- you say how high
 
-mov eax, 0xffffffff
-mov ecx, eax
-mov edx, eax
-mov ebx, eax
-mov ebp, eax
-mov esi, eax
+mov esp, 0x320
+mov ecx, 0x400
 
-mov edi, 0xd
-mov esp, 0x400
-mov [edi], 0x20fc8360
-mov [edi+4], 0xff600374
-mov [edi+8], 0x0400bce7
-mov [edi+12], 0xe7ff0000
-;mov [edi], 0xe7ff6060
-jmp edi
+mov edi, 0x83606060
+mov esi, 0x4c0f00fc
+mov ebp, 0xe4ff60e1
+mov ebx, 0x000420bc ; jmp 0x420
+mov edx, 0xe4ff     ; 
+mov eax, 0xffffffff ; some bad instrs
+pushad
+jmp esp
